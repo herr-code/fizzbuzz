3f7de0ca84bc3870015bafe6e236171b512a0dff
herr-code <herrwebj@gmail.com>
2022-05-08 02:13:26 -0500

    Agregando tests para metodo FizzBuzzService/applyValidationInNumber()

--- test/services/FizzbuzzService.test.js
+++ test/services/FizzbuzzService.test.js
@@ -1,6 +1,6 @@
 const FizzbuzzService = require("./../../lib/services/FizzbuzzService");
 
-describe("Tests para Fizzbuzz Service", () => {
+describe("1) Tests para FizzbuzzService applyValidationInExplorer()", () => {
     test("Debe imprimir mensaje de error si recibe un valor que no sea número (explorer.score)", () => {
         const expected = "Error: The argument must be a number";
         const result = FizzbuzzService.applyValidationInExplorer({name: "Explorer", score: undefined});
@@ -47,5 +47,49 @@ describe("Tests para Fizzbuzz Service", () => {
         const expected = "FIZZBUZZ";
         const result = FizzbuzzService.applyValidationInExplorer(explorer15);
         expect(expected).toBe(result.trick);
+    });
+});
+
+describe("2) Tests para FizzbuzzService applyValidationInNumber()", () => {
+    test("Debe imprimir mensaje de error si recibe un valor que no sea número", () => {
+        const expected = "Error: The argument must be a number";
+        const result = FizzbuzzService.applyValidationInNumber(undefined);
+        expect(expected).toBe(result);
+    });
+
+    test("Debe imprimir el mismo valor si no cumple con las reglas de abajo", () => {
+        const expected = 0;
+        const result = FizzbuzzService.applyValidationInNumber(0);
+        expect(expected).toBe(result);
+    });
+
+    test("Debe imprimir FIZZ si recibe 3", () => {
+        const expected = "FIZZ";
+        const result = FizzbuzzService.applyValidationInNumber(3);
+        expect(expected).toBe(result);
+    });
+
+    test("Debe imprimir FIZZ si recibe un multiplo de 3", () => {
+        const expected = "FIZZ";
+        const result = FizzbuzzService.applyValidationInNumber(6);
+        expect(expected).toBe(result);
+    });
+
+    test("Debe imprimir BUZZ si recibe 5", () => {
+        const expected = "BUZZ";
+        const result = FizzbuzzService.applyValidationInNumber(5);
+        expect(expected).toBe(result);
+    });
+
+    test("Debe imprimir BUZZ si recibe un multiplo de 5", () => {
+        const expected = "BUZZ";
+        const result = FizzbuzzService.applyValidationInNumber(10);
+        expect(expected).toBe(result);
+    });
+
+    test("Debe imprimir FIZZBUZZ si recibe un multiplo de 3 y 5 (explorer.score)", () => {
+        const expected = "FIZZBUZZ";
+        const result = FizzbuzzService.applyValidationInNumber(15);
+        expect(expected).toBe(result);
     });
 });
