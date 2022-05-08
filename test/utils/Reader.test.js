01070b1e2b4ded62b058b005c4dd5ba9797984ab
herr-code <herrwebj@gmail.com>
2022-05-07 19:36:32 -0500

    Creacion de la clase ExplorerController y ExplorerController.test

--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -1,9 +1,8 @@
-const Reader = require('./../../lib/utils/Reader');
+const Reader = require("./../../lib/utils/Reader");
 
 describe("Tests para Reader utils", () => {
     test("No debe ser null ni undefined ni vacia la respuesta de readJsonFile", () => {
-        const result = Reader.readJsonFile('./test/utils/data-example/data-2.json');
-        console.log(result)
+        const result = Reader.readJsonFile("./test/utils/data-example/data-2.json");
         expect(result).not.toBeNull();
         expect(result).not.toBeUndefined();
         expect(result.length).toBeGreaterThan(0);
@@ -11,7 +10,7 @@ describe("Tests para Reader utils", () => {
 
     test("Debe imprimir Leanne Graham para el primer name de users", () => {
         const expected = "Leanne Graham";
-        const result = Reader.readJsonFile('./test/utils/data-example/data-2.json');
+        const result = Reader.readJsonFile("./test/utils/data-example/data-2.json");
         expect(expected).toBe(result[0].name);
     });
 });
